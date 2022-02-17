26add74044313e6c4122fdd4aa636807b5f0a215
Elena Perez <eperez@edgelab.ch>
2022-02-17 16:02:20 +0100

    update: tidied up code and added the get_bypassers function to return the name of the bypasser, the repo by passed and how many times

--- github.py
+++ github.py
@@ -1,4 +1,9 @@
-from typing import List
+from collections import defaultdict
+from email.policy import default
+import json
+import jwt
+import time
+from typing import Dict, List
 from http import HTTPStatus
 import requests
 import os
@@ -29,7 +34,48 @@ def count_dependabot_prs(session: requests.Session, repo_name: str) -> int:
     return update_pr_count
 
 
+def get_bypassers(session: requests.Session) -> dict[str, dict[str, int]]:
+    """Returns the name of the bypasser, the repo bypassed and how many times"""
+    branch_bypasses = defaultdict(lambda: defaultdict(int))
+
+    with session.get(f"{API_URL}/orgs/edgelaboratories/audit-log", params={"per_page": 100, "phrase": "action:protected_branch.policy_override"}) as resp:
+        resp.raise_for_status()
+        next_page = resp.links.get("next")
+        if next_page is not None:
+            print("TODO: There is other pages to process")
+        logs = resp.json()
+
+        for log in logs:
+            if log['action'] == 'protected_branch.policy_override':
+                # branch_bypass_actors[log['actor']] = branch_bypass_actors.get(
+                #     log['actor'], 0) + 1
+                branch_bypasses[log['actor']][log['repo']] += 1
+
+    return branch_bypasses
+    # TODO:
+    # count how many override per actor
+    # [
+    #   {
+    #     "repo": "edgelaboratories/stacks",
+    #     "actor": "vthiery",
+    #     "org": "edgelaboratories",
+    #     "branch": "refs/heads/master",
+    #     "action": "protected_branch.policy_override",
+    #     "@timestamp": 1641796648783,
+    #     "_document_id": "pCtl2n0J8njxnR8oUOEpuw",
+    #     "created_at": 1641796648783,
+    #     "actor_location": {
+    #     "country_code": "CH"
+    #     }
+    #   }
+    # ]
+    # {"vthiery": {"edgelaboratories/stacks": 3, "another": 5}}
+    print(f"There are {len(logs)} to process")
+    print(f"Elena, do your job :p ")
+
+
 def get_repo_info(session: requests.Session, content: dict) -> dict:
+    """Returns """
     repo = {"name": content["name"]}
 
     with session.get(f"{API_URL}/repos/edgelaboratories/{repo['name']}/contents/.github/dependabot.yml") as resp:
@@ -60,13 +106,50 @@ def get_repo_info(session: requests.Session, content: dict) -> dict:
     return repo
 
 
-def main():
-    """Calls the GitHub API to output the number of update PRs, the table data"""
-    token = os.environ['GITHUB_TOKEN']
+def get_github_token() -> str:
+    github_app_id = 172532
+    github_org = "edgelaboratories"
+    now = int(time.time())
+    payload = {
+        # issued at time, 60 seconds in the past to allow for clock drift
+        "iat": now - 60,
+        # JWT expiration time (10 minute maximum)
+        "exp": now + 60,
+        # GitHub App's identifier
+        "iss": github_app_id,
+    }
 
-    session = requests.Session()
-    session.headers["Authorization"] = f"Token {token}"
+    with open("./github.pem", "r") as f:
+        private_key = f.read()
 
+    token = jwt.encode(payload, private_key, algorithm="RS256")
+
+    with requests.get(
+        f"{API_URL}/app/installations", headers={"Authorization": f"Bearer {token}"}
+    ) as resp:
+        resp.raise_for_status()
+        installations = resp.json()
+
+    for install in installations:
+        if install['account']['login'] == github_org:
+            install_id = install['id']
+            break
+    else:
+        raise ValueError(
+            f"could not find installation for the app {github_app_id} in {github_org} org"
+        )
+
+    with requests.post(
+        f"{API_URL}/app/installations/{install_id}/access_tokens", headers={"Authorization": f"Bearer {token}"}
+    ) as resp:
+        resp.raise_for_status()
+        content = resp.json()
+
+    token = content["token"]
+    return token
+
+
+def check_repositories(session: requests.Session):
     resp = session.get(f"{API_URL}/orgs/edgelaboratories/repos")
     resp.raise_for_status()
 
@@ -78,18 +161,26 @@ def main():
 
         if content["archived"] == False:
             repos.append(get_repo_info(session, content))
-    # pprint(content)
 
+    return repos, content
+
+
+def main():
+    """Calls the GitHub API to output the number of update PRs, the table data"""
+    token = get_github_token()
+
+    session = requests.Session()
+    session.headers["Authorization"] = f"Token {token}"
+
+    repos, content = check_repositories(session)
+    get_repo_info(session, content)
     update_prs = count_dependabot_prs(session, content["name"])
     print(
         f'There are currently {update_prs} update PRs in repos with an active dependabot')
 
-    get_repo_info(session, content)
-
     first_table = table(repos)
     print(first_table)
-
-    # pprint(active_bot_repos[0])
+    get_bypassers(session)
 
 
 if __name__ == "__main__":
